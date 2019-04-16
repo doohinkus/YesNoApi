c248f46b886fb86efa8374d2a8c40523725e88da
doohinkus <doohinkus@gmail.com>
2019-04-16 09:00:18 -0700

    refactor api calls

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,58 +1,25 @@
-const axios = require('axios');
-const key = require('../../secret');
-const giphyUrl =`http://api.giphy.com/v1/gifs/search?`;
 const functions = require('../functions');
 
 exports.yesNo = (req, res, next) =>{
-  const answer = functions.chance() == 1 ? "Yes" : "No";
-  const randomGiph = functions.randomGiph(25);
-  let gif ='';
-  // 25 gifs
-  axios.get(`${giphyUrl}q=${answer}&api_key=${key}`)
-    .then(response => {
-      gif = response.data.data[randomGiph].images.downsized_large.url;
+  functions.returnAnswer('Yes', 'No', 25)
+    .then(data => {
+     res.json(data);
     })
-    .catch(function (error) {
-      console.log(error);
-      res.json({ answer, gif: 'images/broken.gif'})
-   })
-   .then(()=>{
-     res.json({ answer, gif });
-   });
+    .catch(err => console.log(err));
 };
 
 exports.flip = (req, res, next) =>{
-  const answer = functions.chance() == 1 ? "Heads" : "Tails";
-  const randomGiph = functions.randomGiph(25);
-  let gif ='';
-  // 25 gifs
-  axios.get(`${giphyUrl}q=${answer}&api_key=${key}`)
-    .then(response => {
-      gif = response.data.data[randomGiph].images.downsized_large.url;
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.json({ answer, gif: 'images/broken.gif'})
-   })
-    .then(()=>{
-       res.json({ answer, gif });
-    });
+  functions.returnAnswer('Heads', 'Tails', 25)
+  .then(data => {
+   res.json(data);
+  })
+  .catch(err => console.log(err));
 };
 
 exports.traffic = (req, res, next) =>{
-  const answer = functions.chance() == 1 ? "Go" : "Stop";
-  const randomGiph = functions.randomGiph(25);
-  let gif ='';
-  // 25 gifs
-  axios.get(`${giphyUrl}q=${answer}&api_key=${key}`)
-    .then(response => {
-      gif = response.data.data[randomGiph].images.downsized_large.url;
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.json({ answer, gif: 'images/broken.gif'})
-   })
-   .then(()=>{
-     res.json({ answer, gif });
-   });
+  functions.returnAnswer('Stop', 'Go', 25)
+  .then(data => {
+   res.json(data);
+  })
+  .catch(err => console.log(err));
 };
--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,29 @@
 
-exports.chance = () =>{
+const axios = require('axios');
+const key = require('../secret');
+const giphyUrl =`http://api.giphy.com/v1/gifs/search?`;
+
+const chance = () =>{
   return Math.round(Math.random()) === 1;
+};
+const randomGiph = (num) => {
+   return Math.round(Math.random() * num);
 }
-exports.randomGiph = num => {
-   return Math.round(Math.random()*num);
+
+exports.returnAnswer = (optionA, optionB, gifs) => { 
+  const answer = chance() == 1 ? optionA : optionB;
+  const gif = randomGiph(gifs);
+  // 25 gifs
+  return axios
+    .get(`${giphyUrl}q=${answer}&api_key=${key}`)
+    .then(response => {
+      return {
+        answer,
+        gif: response.data.data[gif].images.downsized_large.url
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      return { answer, gif: ''};
+    });
 }
